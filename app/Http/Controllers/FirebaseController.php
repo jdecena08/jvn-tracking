4eeddfe85081df4f6a12eaa1d432c50a38fff42f
jvn-jeferson <jbonayon15@gmail.com>
2022-08-29 10:05:20 +0800

    testing firebase on live

--- app/Http/Controllers/FirebaseController.php
+++ app/Http/Controllers/FirebaseController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Kreait\Firebase;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
 
 class FirebaseController extends Controller
 {
